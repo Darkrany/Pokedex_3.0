183b468d21ba18700644fae94da3882598047e37
Satgoltratec <116006255+Satgoltratec@users.noreply.github.com>
2023-12-27 18:11:00 +0100

    imagenes lista

--- src/components/PokedexList.js
+++ src/components/PokedexList.js
@@ -2,21 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function PokedexList({ pokemons, onFetchNextPage }) {
-
-
-
   return (
     <>
-
       <div className="poke-list">
         {pokemons?.pages.map((page) => {
           return page.results.map((pokemon) => {
             return (
-              
               <div className="card-container">
-                <span className="poke-number">#</span>
-                <img className="poke-image" alt={pokemon.name}></img>
-                <h2 className="poke-name">{pokemon.name}</h2>
+                <span className="poke-number">
+                  #{pokemon.url.split("/")[6]}
+                </span>
+                <img
+                  className="poke-image"
+                  alt={pokemon.name}
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                    pokemon.url.split("/")[6]
+                  }.png`}
+                ></img>
+                <h2 className="poke-name">{pokemon.name.charAt().toUpperCase() + pokemon.name.substring(1)}</h2>
               </div>
             );
           });
